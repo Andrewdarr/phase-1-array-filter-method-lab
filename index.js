88d005a2b1d1285e49b9249388de1c56020a4415
Andrew Darr <kildar07@gmail.com>
2022-08-09 21:04:21 -0500

    commit tree e1432d1a8a2ae5ea11c501cfae0f705d7c727fbf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// function findMatching(drivers, str) {
+//         return drivers.filter(driver => {
+//         drivers.toLowerCase() === str.toLowerCase()
+//     });
+// }    
+// function findMatching(arr, str) {
+//     let driver = driver.filter(driver => driverName === str)
+//     return arr.filter(function(str)}
+//     return arr
+//     });
+// }
+
+const findMatching = (array, name) => {
+    const newArray = array.filter(name2 => name2.toUpperCase() === name.toUpperCase())
+    return newArray
+}
